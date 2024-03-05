6a799636d15506d18f1d25e5d7117a373ed416d0
ErickRamirezO <87491977+ErickRamirezO@users.noreply.github.com>
2024-03-05 13:48:26 +0000

    Test para todas las peticiones de cursos y temas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CursosModule } from './cursos/cursos.module';
-import { TemasModule} from './Temas/temas.module'
+import { CursosModule } from './Cursos/cursos.module';
+import { TemasModule } from './Temas/temas.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://electrictiesto:OzRIMlCDq5OhRVBI@cluster0.c9wxd9o.mongodb.net/ExamenWeb?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot(
+      'mongodb+srv://electrictiesto:OzRIMlCDq5OhRVBI@cluster0.c9wxd9o.mongodb.net/ExamenWeb?retryWrites=true&w=majority&appName=Cluster0',
+    ),
     CursosModule,
     TemasModule,
   ],
 })
 export class AppModule {}
-
